16da1a4ec78753b80b08433a9c466a3ef0c44336
纵歌 <dg168550@alibaba-inc.com>
2020-04-28 15:09:59 +0800

    fix relative path

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,6 @@ module.exports = {
   configureWebpack: {
     devtool: process.env.NODE_ENV === 'production' ? false : 'source-map'
   },
-  outputDir: 'docs'
+  outputDir: 'docs',
+  publicPath: ''
 }
